3d677a19745f900be73425845e4eb9875496a882
Liaqat Ali <126480140+Liaqat22@users.noreply.github.com>
2025-11-04 15:10:15 +0500

    Add PhoneNumberInput component for formatted input

---
+++ phone-number-input.tsx
@@ -0,0 +1,60 @@
+'use client'
+
+import * as React from 'react'
+import { TextField } from '@radix-ui/themes'
+import { Controller, useFormContext } from 'react-hook-form'
+import { PatternFormat } from 'react-number-format'
+
+interface PhoneNumberInputProps {
+  name: string
+  size?: React.ComponentProps<typeof TextField.Input>['size']
+  autoFocus?: boolean
+  editable?: boolean
+  placeholder?: string
+  classNames?: string
+  disabled?: boolean
+}
+
+const PhoneNumberInput = ({
+  name,
+  autoFocus,
+  size = '3',
+  editable,
+  placeholder = 'Phone number',
+  classNames,
+  disabled,
+}: PhoneNumberInputProps) => {
+  const form = useFormContext()
+
+  return (
+    <Controller
+      control={form.control}
+      name={name}
+      render={({ field }) => {
+        return (
+          <PatternFormat
+            autoFocus={autoFocus}
+            size={size}
+            id={name}
+            type="text"
+            inputMode="numeric"
+            format="(###) ###-####"
+            mask="_"
+            allowEmptyFormatting={false}
+            placeholder={placeholder}
+            name={name}
+            value={field.value}
+            disabled={disabled}
+            onValueChange={({ value }) => field.onChange(value)}
+            onBlur={field.onBlur}
+            customInput={TextField.Input}
+            readOnly={editable}
+            className={classNames}
+          />
+        )
+      }}
+    />
+  )
+}
+
+export { PhoneNumberInput }
